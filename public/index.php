3775f295ccb03c2e5f53d90dbe777b525f52489b
xandermann <contact@alexandre-hublau.com>
2019-11-23 18:19:57 +0100

    Affiche la hierarchie

--- public/index.php
+++ public/index.php
@@ -2,4 +2,38 @@
 
 require('../src/Donnees.inc.php');
 
-var_dump($Recettes);
+?>
+
+<!--
+<ul>
+    <?php foreach($Recettes as $r): ?>
+        <li><?php var_dump($r) ?></li>
+    <?php endforeach ?>
+</ul>
+-->
+
+<hr>
+
+
+<ul>
+    <?php foreach($Hierarchie as $nom => $hierarchie): ?>
+        <li>Le nom est: "<?= $nom ?>"</li>
+        <li>Sous catégorie:
+            <ul>
+                <?php foreach($hierarchie['sous-categorie'] ?? [] as $sousCategorie): ?>
+                    <li><?= $sousCategorie ?></li>
+                <?php endforeach ?>
+            </ul>
+        </li>
+
+        <li>Super catégorie:
+            <ul>
+                <?php foreach($hierarchie['super-categorie'] ?? [] as $superCategorie): ?>
+                    <li><?= $superCategorie ?></li>
+                <?php endforeach ?>
+            </ul>
+        </li>
+
+        <hr>
+    <?php endforeach ?>
+</ul>
