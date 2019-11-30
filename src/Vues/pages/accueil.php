a442aed5ee8a7e4d26f61edd1c2161d4eb7231eb
xandermann <contact@alexandre-hublau.com>
2019-11-30 21:23:18 +0100

    Maj

--- src/Vues/pages/accueil.php
+++ src/Vues/pages/accueil.php
@@ -1,24 +1,21 @@
 <h1><?= $categorie ?></h1>
 
 <?php if(array_key_exists('super-categorie', $hierarchie)): ?>
-	<h2>Revenir en arrière</h2>
+	<h2>Super catégorie</h2>
 	<ul>
-		<?php foreach($hierarchie['super-categorie'] ?? [] as $superCategorie): ?>
-			<li><?= $superCategorie ?></li>
+		<?php foreach($hierarchie['super-categorie'] as $categorie): ?>
+			<li><a href="index.php?page=accueil&categorie=<?= $categorie ?>"><?= $categorie ?></a></li>
 		<?php endforeach; ?>
 	</ul>
 <?php endif ?>
 
-<h2>Voir un aliment plus précis</h2>
-<ul>
-	<?php foreach($hierarchie as $nom => $h): ?>
-		<li><?= $nom ?>
-		<ul>
-			<?php foreach($h as $id => $categorie): ?>
-				<li>ID:<?= $id ?> - <?= $categorie ?> <a href="index.php?page=accueil&categorie=<?= $categorie ?>">LIEN</a></li>
-			<?php endforeach ?>
-		</ul>
-	</li>
-<?php endforeach ?>
-</ul>
 
+<?php if(array_key_exists('sous-categorie', $hierarchie)): ?>
+	<h2>Sous cagégorie</h2>
+
+	<ul>
+		<?php foreach($hierarchie['sous-categorie'] as $id => $categorie): ?>
+			<li><a href="index.php?page=accueil&categorie=<?= $categorie ?>"><?= $id ?> - <?= $categorie ?></a></li>
+		<?php endforeach ?>
+	</ul>
+<?php endif ?>
