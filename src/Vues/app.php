427634a931f6b25a9292ac6cb09902b339b65530
xandermann <contact@alexandre-hublau.com>
2019-11-30 23:53:54 +0100

    Ajout design

--- src/Vues/app.php
+++ src/Vues/app.php
@@ -2,15 +2,41 @@
 <html lang="fr">
 <head>
 	<title><?= $title ?></title>
+	<style>
+		nav {
+			margin-bottom: 100px;
+			border-top: 10px solid #2196f3;
+			background-color: #6ec6ff;
+			padding: 3px 0;
+		}
+		nav ul {
+			display: flex;
+			justify-content: space-around;
+			list-style-type: none
+		}
+		nav ul li a {
+			box-sizing: border-box;
+			padding: 17px;
+			border: 1px solid black;
+			color: black;
+		}
+
+		.box {
+			border: 1px solid black;
+			margin: 10px 0;
+		}
+	</style>
 </head>
 <body>
 
-	<ul>
-		<li><a href="index.php?page=accueil">Page accueil</a></li>
-		<li><a href="index.php?page=inscription">Inscription</a></li>
-		<li><a href="index.php?page=connexion">Connexion</a></li>
-		<li><a href="index.php?page=recette">Mes recettes preferees</a></li>
-	</ul>
+	<nav>
+		<ul>
+			<li><a href="index.php?page=accueil">Page accueil</a></li>
+			<li><a href="index.php?page=recette">Mes recettes preferees</a></li>
+			<li><a href="index.php?page=connexion">Connexion</a></li>
+			<li><a href="index.php?page=inscription">Inscription</a></li>
+		</ul>
+	</nav>
 
 	<?= $content ?>
 </body>
--- src/Vues/pages/accueil.php
+++ src/Vues/pages/accueil.php
@@ -3,51 +3,62 @@
 <p>Todo: Afficher le chemin</p>
 
 <?php if(array_key_exists('super-categorie', $hierarchie)): ?>
-	<h2>Super catégorie</h2>
-	<ul>
-		<?php foreach($hierarchie['super-categorie'] as $superCategorie): ?>
-			<li><a href="index.php?page=accueil&categorie=<?= str_replace(" ", "_", $superCategorie) ?>">
-				<?= $superCategorie ?>
+	<div class="box">
+
+		<h2>Super catégorie</h2>
+		<ul>
+			<?php foreach($hierarchie['super-categorie'] as $superCategorie): ?>
+				<li><a href="index.php?page=accueil&categorie=<?= str_replace(" ", "_", $superCategorie) ?>">
+					<?= $superCategorie ?>
 				</a></li>
-		<?php endforeach; ?>
-	</ul>
+			<?php endforeach; ?>
+		</ul>
+
+	</div>
 <?php endif ?>
 
 
 <?php if(array_key_exists('sous-categorie', $hierarchie)): ?>
-	<h2>Sous cagégorie</h2>
+	<div class="box">
 
-	<ul>
-		<?php foreach($hierarchie['sous-categorie'] as $id => $sousCategorie): ?>
-			<li><a href="index.php?page=accueil&categorie=<?= str_replace(" ", "_", $sousCategorie) ?>">
-				<?= $id ?> - <?= $sousCategorie ?>
-			</a></li>
-		<?php endforeach ?>
-	</ul>
+		<h2>Sous cagégorie</h2>
+
+		<ul>
+			<?php foreach($hierarchie['sous-categorie'] as $id => $sousCategorie): ?>
+				<li><a href="index.php?page=accueil&categorie=<?= str_replace(" ", "_", $sousCategorie) ?>">
+					<?= $id ?> - <?= $sousCategorie ?>
+				</a></li>
+			<?php endforeach ?>
+		</ul>
+	</div>
 <?php endif ?>
 
 
-<hr><hr><hr><hr><hr>
 
-<h2>Liste de toutes les recettes avec <?= $categorie ?></h2>
+<?php if($recettes != []): ?>
+	<div class="box">
+		<h2>Liste de toutes les recettes avec <?= $categorie ?></h2>
 
-<?php foreach($recettes as $id => $recette): ?>
-	<h3><?= $recette['titre'] ?> <a href="index.php?page=ajouter_recette&id=<?= $id ?>">Ajouter cette recette</a></h3>
-	<p><?= $recette['ingredients'] ?></p>
-	<p><?= $recette['preparation'] ?></p>
+		<?php foreach($recettes as $id => $recette): ?>
+			<h3><?= $recette['titre'] ?> <a href="index.php?page=ajouter_recette&id=<?= $id ?>">Ajouter cette recette</a></h3>
+			<p><?= $recette['ingredients'] ?></p>
+			<p><?= $recette['preparation'] ?></p>
 
-	<ul>
-		<?php foreach($recette['index'] as $ingredient): ?>
-			<li><?= $ingredient ?></li>
+			<ul>
+				<?php foreach($recette['index'] as $ingredient): ?>
+					<li><?= $ingredient ?></li>
+				<?php endforeach ?>
+			</ul>
+
+			<?php
+			$file = str_replace(" ", "_", "Photos/$recette[titre].jpg");
+
+			if(file_exists($file)): ?>
+				<img src="<?= $file ?>" alt="<?= $recette['titre'] ?>">
+			<?php endif ?>
+
+			<hr>
 		<?php endforeach ?>
-	</ul>
 
-	<?php
-	$file = str_replace(" ", "_", "Photos/$recette[titre].jpg");
-
-	if(file_exists($file)): ?>
-		<img src="<?= $file ?>" alt="<?= $recette['titre'] ?>">
-	<?php endif ?>
-
-	<hr>
-<?php endforeach ?>
+	</div>
+<?php endif ?>
