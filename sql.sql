fb2b13ed8eb607a2f97208aeb928c357a69a7e13
xandermann <contact@alexandre-hublau.com>
2019-12-03 14:44:31 +0100

    Ajout SQL

---
+++ sql.sql
@@ -0,0 +1,31 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `recettes`;
+CREATE TABLE `recettes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `utilisateur_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `utilisateur_id` (`utilisateur_id`),
+  CONSTRAINT `recettes_ibfk_1` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateurs` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE `utilisateurs` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `pseudo` varchar(16) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
+  `nom` varchar(50) DEFAULT NULL,
+  `prenom` varchar(50) DEFAULT NULL,
+  `sexe` varchar(1) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `naissance` date DEFAULT NULL,
+  `code_postal` int(5) DEFAULT NULL,
+  `tel` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
