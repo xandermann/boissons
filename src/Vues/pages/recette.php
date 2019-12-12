59edc44509a6291f65227dc02546677181d93769
Louis Matuchet <matuchet1u@fst-ats-311-l07.fst.site.univ-lorraine.fr>
2019-12-12 11:54:57 +0100

    ajout 1 ligne

--- src/Vues/pages/recette.php
+++ src/Vues/pages/recette.php
@@ -6,7 +6,7 @@
 			<li>
 				<h2><?= $recette['titre'] ?></h2>
 				<p><a href="index.php?page=supprimer_recette&id=<?= $id ?>">Supprimer</a></p>
-				<p><a href="?page=voir_recette&id=<?= $id ?>">Supprimer toutes les recettes</a></p>
+				<p><a href="?page=voir_recette&id=<?= $id ?>">Voir la recette <?= $recette['titre'] ?></a></p>
 				<!-- <?= $recette['ingredients'] ?> <?= $recette['preparation'] ?> -->
 			</li>
 		<?php endforeach ?>
