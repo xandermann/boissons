f48c152807fd213382520e4ba06cc30e13910f0a
xandermann <contact@alexandre-hublau.com>
2019-12-24 02:56:05 +0100

    Renommage

--- src/Vues/pages/recherche.php
+++ src/Vues/pages/recherche.php
@@ -101,69 +101,69 @@
 
 
 
-	function autocomplete(inp, arr) {
-		var currentFocus
-		inp.addEventListener("input", function(e) {
-			var a, b, i, val = this.value
-			closeAllLists()
+	function autocomplete(champsDeRecherche, arr) {
+		var elementCourant
+		champsDeRecherche.addEventListener("input", function(e) {
+			let a, el, i, val = this.value
+			fermerToutesLesListes()
 			if (!val) { return false}
-				currentFocus = -1
+				elementCourant = -1
 			a = document.createElement("DIV")
 			a.setAttribute("id", this.id + "autocomplete-list")
 			a.setAttribute("class", "autocomplete-items")
 			this.parentNode.appendChild(a)
 			for (i = 0; i < arr.length; i++) {
 				if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-					b = document.createElement("DIV")
-					b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>"
-					b.innerHTML += arr[i].substr(val.length)
-					b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>"
-					b.addEventListener("click", function(e) {
-						inp.value = this.getElementsByTagName("input")[0].value
-						closeAllLists()
+					el = document.createElement("DIV")
+					el.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>"
+					el.innerHTML += arr[i].substr(val.length)
+					el.innerHTML += "<input type='hidden' value='" + arr[i] + "'>"
+					el.addEventListener("click", function(e) {
+						champsDeRecherche.value = this.getElementsByTagName("input")[0].value
+						fermerToutesLesListes()
 					})
-					a.appendChild(b)
+					a.appendChild(el)
 				}
 			}
 		})
-		inp.addEventListener("keydown", function(e) {
+		champsDeRecherche.addEventListener("keydown", function(e) {
 			var x = document.getElementById(this.id + "autocomplete-list")
 			if (x) x = x.getElementsByTagName("div")
 				if (e.keyCode == 40) {
-					currentFocus++
-					setActive(x)
+					elementCourant++
+					mettreActif(x)
 				} else if (e.keyCode == 38) {
-					currentFocus--
-					setActive(x)
+					elementCourant--
+					mettreActif(x)
 				} else if (e.keyCode == 13) {
 					e.preventDefault()
-					if (currentFocus > -1) {
-						if (x) x[currentFocus].click()
+					if (elementCourant > -1) {
+						if (x) x[elementCourant].click()
 					}
 			}
 		})
-		function setActive(x) {
+		function mettreActif(x) {
 			if (!x) return false
-				removeActive(x)
-			if (currentFocus >= x.length) currentFocus = 0
-				if (currentFocus < 0) currentFocus = (x.length - 1)
-					x[currentFocus].classList.add("autocomplete-active")
+				supprimerActif(x)
+			if (elementCourant >= x.length) elementCourant = 0
+				if (elementCourant < 0) elementCourant = (x.length - 1)
+					x[elementCourant].classList.add("autocomplete-active")
 			}
-			function removeActive(x) {
+			function supprimerActif(x) {
 				for (var i = 0; i < x.length; i++) {
 					x[i].classList.remove("autocomplete-active")
 				}
 			}
-			function closeAllLists(elmnt) {
+			function fermerToutesLesListes(elements) {
 				var x = document.getElementsByClassName("autocomplete-items")
 				for (var i = 0; i < x.length; i++) {
-					if (elmnt != x[i] && elmnt != inp) {
+					if (elements != x[i] && elements != champsDeRecherche) {
 						x[i].parentNode.removeChild(x[i])
 					}
 				}
 			}
 			document.addEventListener("click", function (e) {
-				closeAllLists(e.target)
+				fermerToutesLesListes(e.target)
 	 		})
 		}
 
